c8d8d7c30736b8150efd73bac4cb5789ef8a5d75
zhxy-CN <admin@owo.cab>
2025-07-17 15:02:48 +0800

    :art: refactor: 调整 interview 表结构

--- internal/database/struct/interview.go
+++ internal/database/struct/interview.go
@@ -8,6 +8,7 @@ type Interview struct {
 	Position     string    `gorm:"column:position"`
 	Level        string    `gorm:"column:level"`
 	CreatedAt    time.Time `gorm:"column:created_at;autoCreateTime"`
+	StartTime    time.Time `gorm:"column:start_time"`
 	EndTime      time.Time `gorm:"column:end_time"`
 	Conversation string    `gorm:"column:conversation;type:json"`
 	Facial       string    `gorm:"column:facial;type:json"`
